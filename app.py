7fd57f05d1865d406106072aa26ae209007ad801
fatimafarooq03 <96242090+fatimafarooq03@users.noreply.github.com>
2023-12-07 22:19:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,16 +3,6 @@ import numpy as np
 import pickle
 import pandas as pd
 
-from sklearn.linear_model import LogisticRegression 
-from sklearn.neighbors import KNeighborsClassifier 
-from sklearn.tree import DecisionTreeClassifier, plot_tree
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.metrics import ConfusionMatrixDisplay 
-from sklearn.compose import make_column_transformer 
-from sklearn.pipeline import Pipeline
-from sklearn.linear_model import LinearRegression
-
 st.header('A Model for insurance costs')
 
 st.write('Please enter the Age, Sex, BMI, number of children, smoking status, and region')
