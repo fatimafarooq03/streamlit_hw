ba4124db09b8e2a5bcf1643d092fea35a5e777cf
Fatima Farooq <96242090+fatimafarooq03@users.noreply.github.com>
2024-07-31 14:24:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,26 +3,28 @@ import numpy as np
 import pickle
 import pandas as pd
 
-st.header('A Model for Insurance Costs')
+st.header('A Model for insurance costs')
 
 st.write('Please enter the Age, Sex, BMI, number of children, smoking status, and region')
 
 age = st.number_input('Age')
-sex = st.selectbox('Sex', ['male', 'female'])
+sex = st.selectbox('Sex', ['male', 'female'])  # Use a dropdown for categorical variable
 BMI = st.number_input('BMI')
 children = st.number_input('Number of Children')
-smoker = st.selectbox('Do you smoke?', ['yes', 'no'])
+smoker = st.selectbox('Do you smoke?', ['yes', 'no'])  # Use a dropdown for categorical variable
 region = st.selectbox('Region', ['southwest', 'northwest','southeast','northeast'])
 
-# Create feature vector as a DataFrame
-X = pd.DataFrame([[age, sex, BMI, children, smoker, region]], columns=['age', 'sex', 'bmi', 'children', 'smoker', 'region'])
 
-# Load the trained model
+# Create feature vector
+X = np.array([[age, sex, BMI, children, smoker,region]])
+X = pd.DataFrame(X,columns=['age','sex','bmi','children','smoker','region'])
+
+
+
 with open('insurance_model.pkl', 'rb') as f:
     model = pickle.load(f)
 
-# Make a prediction
 pred = model.predict(X)
 
-# Display the prediction
 st.write(f'The model predicts an insurance cost of $ {pred[0]}')
+ 
